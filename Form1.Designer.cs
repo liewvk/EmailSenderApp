bd214a8da8f2aeb2f5423b79c22433c740f95f21
liewvk <voonkiong@gamil.com>
2026-07-17 20:23:19 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace EmailSenderApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblSmtpServer = new System.Windows.Forms.Label();
+            this.txtSmtpServer = new System.Windows.Forms.TextBox();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.lblSenderEmail = new System.Windows.Forms.Label();
+            this.txtSenderEmail = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.chkShowPassword = new System.Windows.Forms.CheckBox();
+            this.lblRecipientEmail = new System.Windows.Forms.Label();
+            this.txtRecipientEmail = new System.Windows.Forms.TextBox();
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.rtbMessage = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(266, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(250, 38);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Email Sender App";
+            // 
+            // lblSmtpServer
+            // 
+            this.lblSmtpServer.AutoSize = true;
+            this.lblSmtpServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSmtpServer.Location = new System.Drawing.Point(78, 92);
+            this.lblSmtpServer.Name = "lblSmtpServer";
+            this.lblSmtpServer.Size = new System.Drawing.Size(123, 22);
+            this.lblSmtpServer.TabIndex = 1;
+            this.lblSmtpServer.Text = "SMTP Server:";
+            // 
+            // txtSmtpServer
+            // 
+            this.txtSmtpServer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSmtpServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSmtpServer.Location = new System.Drawing.Point(207, 84);
+            this.txtSmtpServer.Name = "txtSmtpServer";
+            this.txtSmtpServer.Size = new System.Drawing.Size(185, 30);
+            this.txtSmtpServer.TabIndex = 2;
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPort.Location = new System.Drawing.Point(427, 92);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(48, 22);
+            this.lblPort.TabIndex = 1;
+            this.lblPort.Text = "Port:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPort.Location = new System.Drawing.Point(481, 84);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(185, 30);
+            this.txtPort.TabIndex = 2;
+            // 
+            // lblSenderEmail
+            // 
+            this.lblSenderEmail.AutoSize = true;
+            this.lblSenderEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenderEmail.Location = new System.Drawing.Point(79, 147);
+            this.lblSenderEmail.Name = "lblSenderEmail";
+            this.lblSenderEmail.Size = new System.Drawing.Size(122, 22);
+            this.lblSenderEmail.TabIndex = 1;
+            this.lblSenderEmail.Text = "Sender Email:";
+            // 
+            // txtSenderEmail
+            // 
+            this.txtSenderEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSenderEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenderEmail.Location = new System.Drawing.Point(207, 147);
+            this.txtSenderEmail.Name = "txtSenderEmail";
+            this.txtSenderEmail.Size = new System.Drawing.Size(185, 30);
+            this.txtSenderEmail.TabIndex = 2;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(422, 155);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(94, 22);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(522, 147);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(185, 30);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // chkShowPassword
+            // 
+            this.chkShowPassword.AutoSize = true;
+            this.chkShowPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkShowPassword.Location = new System.Drawing.Point(431, 196);
+            this.chkShowPassword.Name = "chkShowPassword";
+            this.chkShowPassword.Size = new System.Drawing.Size(161, 26);
+            this.chkShowPassword.TabIndex = 3;
+            this.chkShowPassword.Text = "Show Password";
+            this.chkShowPassword.UseVisualStyleBackColor = true;
+            this.chkShowPassword.CheckedChanged += new System.EventHandler(this.chkShowPassword_CheckedChanged);
+            // 
+            // lblRecipientEmail
+            // 
+            this.lblRecipientEmail.AutoSize = true;
+            this.lblRecipientEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecipientEmail.Location = new System.Drawing.Point(62, 245);
+            this.lblRecipientEmail.Name = "lblRecipientEmail";
+            this.lblRecipientEmail.Size = new System.Drawing.Size(139, 22);
+            this.lblRecipientEmail.TabIndex = 1;
+            this.lblRecipientEmail.Text = "Recipient Email:";
+            // 
+            // txtRecipientEmail
+            // 
+            this.txtRecipientEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRecipientEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRecipientEmail.Location = new System.Drawing.Point(207, 237);
+            this.txtRecipientEmail.Name = "txtRecipientEmail";
+            this.txtRecipientEmail.Size = new System.Drawing.Size(234, 30);
+            this.txtRecipientEmail.TabIndex = 2;
+            // 
+            // lblSubject
+            // 
+            this.lblSubject.AutoSize = true;
+            this.lblSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubject.Location = new System.Drawing.Point(126, 288);
+            this.lblSubject.Name = "lblSubject";
+            this.lblSubject.Size = new System.Drawing.Size(75, 22);
+            this.lblSubject.TabIndex = 1;
+            this.lblSubject.Text = "Subject:";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSubject.Location = new System.Drawing.Point(207, 280);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(234, 30);
+            this.txtSubject.TabIndex = 2;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(62, 337);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(87, 22);
+            this.lblMessage.TabIndex = 1;
+            this.lblMessage.Text = "Message:";
+            // 
+            // rtbMessage
+            // 
+            this.rtbMessage.Location = new System.Drawing.Point(66, 372);
+            this.rtbMessage.Name = "rtbMessage";
+            this.rtbMessage.Size = new System.Drawing.Size(700, 180);
+            this.rtbMessage.TabIndex = 4;
+            this.rtbMessage.Text = "";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(66, 582);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(138, 40);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send Email";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(238, 582);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(114, 40);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(685, 583);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(115, 39);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(400, 591);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(62, 22);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Ready";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 653);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.rtbMessage);
+            this.Controls.Add(this.chkShowPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.txtRecipientEmail);
+            this.Controls.Add(this.txtSenderEmail);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtSmtpServer);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblSubject);
+            this.Controls.Add(this.lblRecipientEmail);
+            this.Controls.Add(this.lblSenderEmail);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblSmtpServer);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Email Sender App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblSmtpServer;
+        private System.Windows.Forms.TextBox txtSmtpServer;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label lblSenderEmail;
+        private System.Windows.Forms.TextBox txtSenderEmail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.CheckBox chkShowPassword;
+        private System.Windows.Forms.Label lblRecipientEmail;
+        private System.Windows.Forms.TextBox txtRecipientEmail;
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.RichTextBox rtbMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+
+
+namespace EmailSenderApp
+{
+    public partial class Form1 : Form
+    {
+        private bool IsValidEmail(string email)
+        {
+            return email.Contains("@") && email.Contains(".");
+        }
+        private bool ValidateInput(out int port)
+        {
+            port = 0;
+
+            if (txtSmtpServer.Text.Trim() == "")
+            {
+                MessageBox.Show("Please enter the SMTP server.",
+                                "Missing SMTP Server",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtSmtpServer.Focus();
+                return false;
+            }
+
+            if (!int.TryParse(txtPort.Text.Trim(), out port))
+            {
+                MessageBox.Show("Please enter a valid SMTP port.",
+                                "Invalid Port",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtPort.Focus();
+                return false;
+            }
+
+            if (port <= 0)
+            {
+                MessageBox.Show("SMTP port must be greater than zero.",
+                                "Invalid Port",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtPort.Focus();
+                return false;
+            }
+
+            if (txtSenderEmail.Text.Trim() == "")
+            {
+                MessageBox.Show("Please enter the sender email.",
+                                "Missing Sender Email",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtSenderEmail.Focus();
+                return false;
+            }
+
+            if (!IsValidEmail(txtSenderEmail.Text.Trim()))
+            {
+                MessageBox.Show("Please enter a valid sender email.",
+                                "Invalid Sender Email",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtSenderEmail.Focus();
+                return false;
+            }
+
+            if (txtPassword.Text.Trim() == "")
+            {
+                MessageBox.Show("Please enter the sender password or app password.",
+                                "Missing Password",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtPassword.Focus();
+                return false;
+            }
+
+            if (txtRecipientEmail.Text.Trim() == "")
+            {
+                MessageBox.Show("Please enter the recipient email.",
+                                "Missing Recipient Email",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtRecipientEmail.Focus();
+                return false;
+            }
+
+            if (!IsValidEmail(txtRecipientEmail.Text.Trim()))
+            {
+                MessageBox.Show("Please enter a valid recipient email.",
+                                "Invalid Recipient Email",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtRecipientEmail.Focus();
+                return false;
+            }
+
+            if (txtSubject.Text.Trim() == "")
+            {
+                MessageBox.Show("Please enter the email subject.",
+                                "Missing Subject",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtSubject.Focus();
+                return false;
+            }
+
+            if (rtbMessage.Text.Trim() == "")
+            {
+                MessageBox.Show("Please enter the email message.",
+                                "Missing Message",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                rtbMessage.Focus();
+                return false;
+            }
+
+            return true;
+        }
+        private void ResetAllFields()
+        {
+            txtSmtpServer.Clear();
+            txtPort.Text = "587";
+            txtSenderEmail.Clear();
+            txtPassword.Clear();
+            txtRecipientEmail.Clear();
+            txtSubject.Clear();
+            rtbMessage.Clear();
+
+            chkShowPassword.Checked = false;
+            lblStatus.Text = "Ready";
+
+            txtSmtpServer.Focus();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            txtSmtpServer.Text = "your-real-smtp-server.com";
+            txtPort.Text = "465";
+
+            lblStatus.Text = "Ready";
+            txtSmtpServer.Focus();
+
+        }
+
+        private void chkShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+
+            txtPassword.UseSystemPasswordChar = !chkShowPassword.Checked;
+
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInput(out int port))
+            {
+                return;
+            }
+
+            string smtpServer = txtSmtpServer.Text.Trim();
+            string senderEmail = txtSenderEmail.Text.Trim();
+            string password = txtPassword.Text.Trim();
+            string recipientEmail = txtRecipientEmail.Text.Trim();
+            string subject = txtSubject.Text.Trim();
+            string messageBody = rtbMessage.Text.Trim();
+
+            try
+            {
+                btnSend.Enabled = false;
+                lblStatus.Text = "Sending email...";
+
+                MimeMessage message = new MimeMessage();
+
+                message.From.Add(new MailboxAddress("Sender", senderEmail));
+                message.To.Add(new MailboxAddress("Recipient", recipientEmail));
+                message.Subject = subject;
+
+                message.Body = new TextPart("plain")
+                {
+                    Text = messageBody
+                };
+
+                using (SmtpClient client = new SmtpClient())
+                {
+                    client.CheckCertificateRevocation = false;
+                    await client.ConnectAsync(
+    smtpServer,
+    port,
+    SecureSocketOptions.SslOnConnect);
+
+                    await client.AuthenticateAsync(senderEmail, password);
+
+                    await client.SendAsync(message);
+
+                    await client.DisconnectAsync(true);
+                }
+
+                lblStatus.Text = "Email sent successfully.";
+
+                MessageBox.Show("Email sent successfully.",
+                                "Success",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "Failed to send email.";
+
+                MessageBox.Show("Failed to send email.\n\n" + ex.Message,
+                                "Email Error",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+            finally
+            {
+                btnSend.Enabled = true;
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtRecipientEmail.Clear();
+            txtSubject.Clear();
+            rtbMessage.Clear();
+
+            lblStatus.Text = "Ready";
+
+            txtRecipientEmail.Focus();
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to exit?",
+                                      "Confirm Exit",
+                                      MessageBoxButtons.YesNo,
+                                      MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+        }
+    }
+}
